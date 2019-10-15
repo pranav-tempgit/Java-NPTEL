daede101dbc1734f3886969d063bcefc8560c9a3
pranav-tempgit <56608263+pranav-tempgit@users.noreply.github.com>
2019-10-16 01:14:58 +0530

    Update Week12Q1.java

--- Week12Q1.java
+++ Week12Q1.java
@@ -1,3 +1,5 @@
+//Source : https://github.com/kameshsr/Nptel_Java/blob/master/week12/Assignment1_AdvanceCalculator.java
+
 import java.util.Scanner;
 public class Question92{
 	public static void main(String args[]){
@@ -10,26 +12,21 @@ public class Question92{
     char seq[] = input.toCharArray();
 		int outflag=0;
 		
-		// Start the mapping process for each input character
+		
 		for(int i=0; i<seq.length; i++){
 			seq[i]=gui_map(seq[i]);
 		}
 		
-		//Print Mapped GUI (remove comment to see the mapped sequence input)
-		/*
-		for(int i=0; i<seq.length; i++){
-			System.out.print(seq[i]);
-		}
-		*/
 		
-		// Use double type of values for entire calculation
+		
+		
 		double operand1=0.0;
 		String o1="";
 		double operand2=0.0;
 		String o2="";
 		double output=0.0;
 		
-		// Perform calculaton operations
+		
 		outerloop:
 		for(int i=0; i<seq.length; i++){
 			int r=0;
@@ -71,7 +68,7 @@ public class Question92{
 			}
 		}
 		
-		// Check if output is available and print the output
+		
 		if(outflag==1)
 			System.out.print(output);
       
